807aca600afcc75fdf182df9da9f7d0ed3f05454
Charles Nicollas <charles.nicollas@cdbdx.biz>
2023-07-19 16:49:19 +0200

    chore: configure linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,30 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:testing-library/react",
+    "plugin:security/recommended",
+    "plugin:xss/recommended",
+    "prettier",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  overrides: [
+    {
+      files: ["bin/*.js", "lib/*.js"],
+    },
+  ],
+  ignorePatterns: ["dist", "storybook-static", "coverage"],
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "simple-import-sort"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
