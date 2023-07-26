07c00817f4c698cee959ed43933d50056fbc0c0b
Charles Nicollas <charles.nicollas@cdbdx.biz>
2023-07-26 13:51:06 +0200

    fix: 🐛 add css baseline

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,7 +5,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./queryClient";
@@ -14,6 +14,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
