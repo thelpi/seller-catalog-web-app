64b7bfa2eedb8891d8eaa1f63e5d899d7ecaced6
Charles Nicollas <charles.nicollas@cdbdx.biz>
2023-07-27 16:02:17 +0200

    test: 💍 price test using stubGlobal

---
+++ src/ds/atoms/Price/Price.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Price from "./Price.jsx";
+import { vi } from "vitest";
+
+afterEach(() => {
+  vi.unstubAllGlobals();
+});
+
+describe("Price", () => {
+  test("in us-US and dollar displays the price and the currency", () => {
+    render(<Price value={100} currency="USD" />);
+
+    expect(screen.getByText("$100.00")).toBeInTheDocument();
+  });
+
+  test("in fr-FR and euro displays the price and the currency", () => {
+    vi.stubGlobal("navigator", { language: "fr-FR" });
+    render(<Price value={100} />);
+
+    expect(screen.getByText("100,00 €")).toBeInTheDocument();
+  });
+});
